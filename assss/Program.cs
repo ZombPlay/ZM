ba8b30e24f4adca15ec5af2944f5d8f728526fa4
ZombPlay <113186794+ZombPlay@users.noreply.github.com>
2022-09-21 21:05:14 +0300

    Add files via upload
    Это практическая №4, которую мне помогла сделать Жаба >:D

---
+++ assss/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Введите два числа которые будут меньше 10");
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+double y = 0;
+
+switch ((a * b) / (a + b))
+{
+    case 0:
+        y = 1 / (a + b);
+        break;
+
+    case 1:
+    case 2:
+        y = (a + b) / (b * b);
+        break;
+
+    case 3:
+        y = (a * a) + (b * b);
+        break;
+
+    case 4:
+        y = b * Math.Cos(a);
+        break;
+
+    case 5:
+        y = a * Math.Sin(b);
+        break;
+
+    default:
+        y = 0;
+        break;
+
+}
+
+Console.WriteLine($"Y равен = {y}");
