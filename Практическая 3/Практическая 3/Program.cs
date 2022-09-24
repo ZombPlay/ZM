68bbf3abeef61a8526fd7b5b1c75088a2f2fa7dd
ZombPlay <113186794+ZombPlay@users.noreply.github.com>
2022-09-24 12:03:47 +0300

    Add files via upload

--- Практическая 3/Практическая 3/Program.cs
+++ Практическая 3/Практическая 3/Program.cs
@@ -1,29 +1,29 @@
-﻿double y;
-double d = 0;
-Console.Write("Введите первое число: ");
-double a = double.Parse(Console.ReadLine());
-Console.Write("Введите второе число: ");
-double b = double.Parse(Console.ReadLine());
-Console.Write("Введите третье число: ");
-double c = double.Parse(Console.ReadLine());
-if (a != 0 && b != 0 && c != 0)
-{
-    d = 1 / a + 1 / b + 1 / c;
-    if (d > 0 && a > 0)
-    {
-        y = Math.Sqrt(d);
-    }
-    else
-    {
-        y = 2;
-    }
-    Console.WriteLine($"y={y}");
+﻿Console.Write("Введите первое число: ");
+int a= int.Parse(Console.ReadLine());
 
+Console.Write("Введите второе число: ");
+int b= int.Parse(Console.ReadLine());
+
+Console.Write("Введите 3 число: ");
+int c= int.Parse(Console.ReadLine());
+
+double y = 0;
+double x = 0;
+
+if (a > b && b > c)
+{
+  x=0.2;
+    y = (x * x) + 0.6 * x + Math.Sin(x / 2);
+}
+else if (b<c && a<c)
+{
+    x = 2;
+    y = Math.Pow(x, 3) + (x * x) + x + 1;
 }
 else
 {
-    Console.WriteLine("Нет решения!");
+    x = 0;
+    y = 0;
 }
-
-
-
+Console.WriteLine($"X = {x}");
+Console.WriteLine($"Y = {y}");
